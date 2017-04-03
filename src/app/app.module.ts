e136b5d6589a96f312e08aa0301716cea016441a
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-04-03 15:39:13 +0200

    build: Import Bootstrap 4 and ng-bootstrap as dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,8 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    AppRoutingModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
