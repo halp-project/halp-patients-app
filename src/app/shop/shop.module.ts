418dd0272ccb595f426d13a74ad868dff18f5a40
georiord <giorior95@gmail.com>
2017-04-05 00:29:49 +0200

    Front-End: Shop [Changes]

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
-import { ShopGridComponent } from './shop-grid/shop-grid.component';
-import { ShopCardComponent } from './shop-grid/shop-card/shop-card.component';
+import { ProductsGridComponent } from './product-grid/products-grid.component';
+import { ProductCardComponent } from './product-grid/product-card/product-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ShopRoutingModule
   ],
-  declarations: [ ShopComponent, ShopGridComponent, ShopCardComponent ]
+  declarations: [ ShopComponent, ProductsGridComponent, ProductCardComponent ]
 })
 export class ShopModule { }
