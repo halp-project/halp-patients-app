e5d285412af3be8f627a0493bfe214aad0ab06aa
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-05-22 01:44:09 +0200

    feat(auth): Login and logout feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Patient } from './patient';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  patient: Patient = new Patient();
+  errorMessage: string = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    public router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.logIn(this.patient)
+      .then((response) => {
+        localStorage.setItem('id_token', response.json().token);
+        this.router.navigate(['home']);
+      })
+      .catch((error: any) => {
+        if (error.status === 400) {
+          this.errorMessage = 'You must send the username and the password';
+        } else {
+          this.errorMessage = 'The username or password dont match'
+        }
+      });
   }
 
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
   ],
   declarations: [
     AuthComponent
-  ]
+  ],
+  providers: [AuthService]
 })
 export class AuthModule { }
